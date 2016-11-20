ddbc3c01af6977bf95098394f852918cf61b4271
zhukejin1223 <zhukejin@msn.com>
2016-11-20 22:25:38 +0800

    1. update home code

--- src/components/home/index.js
+++ src/components/home/index.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import sty from './home.css';
-import {Carousel, Form, Icon, Input, Button, Select} from 'antd';
+import {Carousel, Form, DatePicker, Input, Button, Select} from 'antd';
 import classNames from 'classnames';
 
 const FormItem = Form.Item;
 const Option = Select.Option;
 const InputGroup = Input.Group;
+const { MonthPicker, RangePicker } = DatePicker;
 
 function handleChange(value) {
     console.log(`selected ${value}`);
@@ -37,15 +38,21 @@ const Home = Form.create()(React.createClass({
         });
     },
     handleSearch() {
-        if (this.props.onSearch) {
-            this.props.onSearch(this.state.value);
-        }
+        console.log(123)
     },
     render() {
-        const {getFieldDecorator} = this.props.form;
-
+        const btnCls = classNames({
+            'ant-search-btn': true,
+            'ant-search-btn-noempty': !!this.state.value.trim(),
+        });
+        const searchCls = classNames({
+            'ant-search-input': true,
+            'ant-search-input-focus': this.state.focus,
+        });
         return (
             <div>
+
+                {/* Carousel */}
                 <Carousel autoplay dots={false}>
                     <div>
                         <img width={1280} height={400} src="http://temp.im/1280x400" alt=""/>
@@ -58,33 +65,37 @@ const Home = Form.create()(React.createClass({
                     </div>
                 </Carousel>
 
+                {/* search Bar */}
                 <div className={sty.searchBar}>
-                    <Form inline onSubmit={this.handleSubmit}>
+                    <Form inline onSubmit={this.handleSubmit} style={{margin: '0 auto', width: 620}}>
                         <FormItem>
-                            <Select defaultValue="1" style={{width: 145}} onChange={handleChange}>
+                            <Select size="default" placeholder='类别' style={{width: 145}} onChange={handleChange}>
                                 <Option value="1">类别</Option>
                                 <Option value="2">Lucy</Option>
                                 <Option value="3">Yiminghe</Option>
                             </Select>
                         </FormItem>
                         <FormItem>
-                            <Select defaultValue="1" style={{width: 145}} onChange={handleChange}>
+                            <Select size="default" placeholder="城市" style={{width: 145}} onChange={handleChange}>
                                 <Option value="1">城市</Option>
                                 <Option value="2">Lucy</Option>
                                 <Option value="3">Yiminghe</Option>
                             </Select>
                         </FormItem>
                         <FormItem>
-                            <Input placeholder="日期"/>
+                            <DatePicker size="default" style={{width: 145}}/>
                         </FormItem>
                         <FormItem>
-                            <InputGroup>
-                                <Input value={this.state.value} onPressEnter={this.handleSearch}
-                                />
-                                <div className="ant-input-group-wrap">
-                                    <Button icon="search" onClick={this.handleSearch}/>
-                                </div>
-                            </InputGroup>
+                            <div className="ant-search-input-wrapper" style={{width: 145}}>
+                                <InputGroup className={searchCls}>
+                                    <Input placeholder="关键字" value={this.state.value} onPressEnter={this.handleSearch} />
+                                    <div className="ant-input-group-wrap">
+                                        <Button icon="search" className={btnCls} onClick={this.handleSearch} />
+                                    </div>
+                                </InputGroup>
+                            </div>
+
+                            {/*<Input addonAfter={<Button icon="search" onClick={this.handleSearch}/>} type="password" placeholder="Password" />*/}
                         </FormItem>
                     </Form>
                 </div>
