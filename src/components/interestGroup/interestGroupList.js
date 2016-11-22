97ccb1f049ff9ec291b1e05b85e1aa32403e56be
neo_huang <neo_huang@morecruit.cn>
2016-11-22 08:51:01 +0800

    update interestGroupList

---
+++ src/components/interestGroup/interestGroupList.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+
+class interestGroupList extends React.Component{
+
+
+
+    render(){
+        return(
+            <div>123</div>
+        )
+    }
+}
+
+
+export default interestGroupList
--- src/router.js
+++ src/router.js
@@ -4,6 +4,7 @@ import { Router, Route, IndexRoute } from 'dva/router';
 import Frame from './components/common/frame';
 import Home from './components/home';
 import Login from './components/login';
+import interestGroupList from './components/interestGroup/interestGroupList'
 
 export default function({ history }) {
   return (
@@ -11,6 +12,7 @@ export default function({ history }) {
         <Route name="首页" path="/" components={Frame}>
             <IndexRoute components={Home} />
             <Route path="/home" component={Home} />
+            <Route path="/interestGroupLList" component={interestGroupList}/>
             <Route path="/login" components={Login}></Route>
         </Route>
     </Router>
